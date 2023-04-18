adc0439b967fbe8e47356aa9851d340a868eb5a2
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-04-18 10:03:08 +0300

    update upload_func

--- com/automation/example/upload_func.py
+++ com/automation/example/upload_func.py
@@ -6,15 +6,15 @@ def test_upload_file_to_drive(test_file):
     file_path = os.path.join(os.getcwd(),test_file)
 
     # Получение токена авторизации
-    credentials = Credentials.from_authorized_user_info(info={'access_token': 'ya29.a0Ael9sCPqGq3yW5p4swizZGpuvLB1Qmacie4LnEpcI664ZIEg0aK_OTTd4WjQ8ARKdf8AK5FneDacj-sR0b8VWKWWXDDu50nbyr4M3uTP1BKGRtBdc3DZf1q3IRgsNJJ_eyAQSFo3t8dVnvNxj8YHn2m83czdaCgYKAcMSARISFQF4udJhRA_LnIAeJfQ27DQAeJMpvQ0163',
-                                                              'refresh_token': 'ya29.a0Ael9sCPqGq3yW5p4swizZGpuvLB1Qmacie4LnEpcI664ZIEg0aK_OTTd4WjQ8ARKdf8AK5FneDacj-sR0b8VWKWWXDDu50nbyr4M3uTP1BKGRtBdc3DZf1q3IRgsNJJ_eyAQSFo3t8dVnvNxj8YHn2m83czdaCgYKAcMSARISFQF4udJhRA_LnIAeJfQ27DQAeJMpvQ0163',
-                                                              # 'refresh_token': '1//04r787uASzED9CgYIARAAGAQSNwF-L9Ir-C74zltx2F5Fes7cEys10nU1j5FH_d4Tp1X1sbSVfbahEW2LbZpOIiaL4Ti_1je6N8E',
+    credentials = Credentials.from_authorized_user_info(info={'access_token': 'ya29.a0Ael9sCMLY0OcCy-471KE0n25ywFGKq8gC_mRZJBIhST9x9XguHqX-uwBLQprQNVERReCbpcSD9PQRAsq_0ibLbaINjkSDGCrE_MCwE48D-syeZTefSF52_W5l3V67Gfv88vRnDI55q9FsVgubTR0BOSdukRgaCgYKAUUSARISFQF4udJhyjU_slB_qxA91wPQLhLAiw0163',
+                                                              'refresh_token': 'ya29.a0Ael9sCMLY0OcCy-471KE0n25ywFGKq8gC_mRZJBIhST9x9XguHqX-uwBLQprQNVERReCbpcSD9PQRAsq_0ibLbaINjkSDGCrE_MCwE48D-syeZTefSF52_W5l3V67Gfv88vRnDI55q9FsVgubTR0BOSdukRgaCgYKAUUSARISFQF4udJhyjU_slB_qxA91wPQLhLAiw0163',
+                                                              # 'refresh_token': '1//04qEH7eV1Cj9bCgYIARAAGAQSNwF-L9IrgHKkvHxdsKOtb5mtCWCAh3VDQXmiBmk2jdPfNnhM8v6cdjFjB9V0_k3fwDR34v8zFp0',
                                                               'client_id': '77864409271-ujcgeg2243mca6ek98pn7qahbjjhalga.apps.googleusercontent.com',
                                                               'client_secret': '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',
                                                               'token_uri': 'https://oauth2.googleapis.com/token'})
     headers = {
-        'Authorization': f"Bearer {credentials.token}",
-        'Content-Type': 'application/octet-stream'
+        'Authorization': f"Bearer {credentials.refresh_token}",
+        'Content-Type': 'application/json; charset=utf-8'
     }
 
     url = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
