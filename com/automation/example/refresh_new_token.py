860b4b639d402abc1c00c00e72e341c2673e47b1
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-04-19 19:31:58 +0300

    fixed

---
+++ com/automation/example/refresh_new_token.py
@@ -0,0 +1,21 @@
+import requests
+from constants import Links
+
+def refresh_token(refresh_token):
+    token_url = Links.TOKEN_URL
+    token_data = {
+        'refresh_token': refresh_token,
+        'client_id': Links.CLIENT_ID,
+        'client_secret': Links.CLIENT_SECRET,
+        'grant_type': 'refresh_token',
+    }
+    response = requests.post(token_url, data=token_data)
+    if response.ok:
+        token = response.json()['access_token']
+        print('Токен обновлен успешно.')
+        print('access_token = {}'.format(token))
+        return token
+    else:
+        print('Ошибка при обновлении токена:')
+        print(response.content)
+        return None
--- com/automation/example/upload_func.py
+++ com/automation/example/upload_func.py
@@ -1,26 +1,26 @@
-import os
-import pytest
-import requests
-from google.oauth2.credentials import Credentials
-def test_upload_file_to_drive(test_file):
-    file_path = os.path.join(os.getcwd(),test_file)
-
-    # Получение токена авторизации
-    credentials = Credentials.from_authorized_user_info(info={'access_token': 'ya29.a0Ael9sCMLY0OcCy-471KE0n25ywFGKq8gC_mRZJBIhST9x9XguHqX-uwBLQprQNVERReCbpcSD9PQRAsq_0ibLbaINjkSDGCrE_MCwE48D-syeZTefSF52_W5l3V67Gfv88vRnDI55q9FsVgubTR0BOSdukRgaCgYKAUUSARISFQF4udJhyjU_slB_qxA91wPQLhLAiw0163',
-                                                              'refresh_token': 'ya29.a0Ael9sCMLY0OcCy-471KE0n25ywFGKq8gC_mRZJBIhST9x9XguHqX-uwBLQprQNVERReCbpcSD9PQRAsq_0ibLbaINjkSDGCrE_MCwE48D-syeZTefSF52_W5l3V67Gfv88vRnDI55q9FsVgubTR0BOSdukRgaCgYKAUUSARISFQF4udJhyjU_slB_qxA91wPQLhLAiw0163',
-                                                              # 'refresh_token': '1//04qEH7eV1Cj9bCgYIARAAGAQSNwF-L9IrgHKkvHxdsKOtb5mtCWCAh3VDQXmiBmk2jdPfNnhM8v6cdjFjB9V0_k3fwDR34v8zFp0',
-                                                              'client_id': '77864409271-ujcgeg2243mca6ek98pn7qahbjjhalga.apps.googleusercontent.com',
-                                                              'client_secret': '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',
-                                                              'token_uri': 'https://oauth2.googleapis.com/token'})
-    headers = {
-        'Authorization': f"Bearer {credentials.refresh_token}",
-        'Content-Type': 'application/json; charset=utf-8'
-    }
-
-    url = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
-
-    with open(file_path, 'rb') as file:
-        data = file.read()
-    response = requests.post(url, headers=headers, data=data)
-    return response
-
+# import os
+# import pytest
+# import requests
+# from google.oauth2.credentials import Credentials
+# def test_upload_file_to_drive(test_file):
+#     file_path = os.path.join(os.getcwd(),test_file)
+#
+#     # Получение токена авторизации
+#     credentials = Credentials.from_authorized_user_info(info={'access_token': 'ya29.a0Ael9sCMLY0OcCy-471KE0n25ywFGKq8gC_mRZJBIhST9x9XguHqX-uwBLQprQNVERReCbpcSD9PQRAsq_0ibLbaINjkSDGCrE_MCwE48D-syeZTefSF52_W5l3V67Gfv88vRnDI55q9FsVgubTR0BOSdukRgaCgYKAUUSARISFQF4udJhyjU_slB_qxA91wPQLhLAiw0163',
+#                                                               'refresh_token': 'ya29.a0Ael9sCMLY0OcCy-471KE0n25ywFGKq8gC_mRZJBIhST9x9XguHqX-uwBLQprQNVERReCbpcSD9PQRAsq_0ibLbaINjkSDGCrE_MCwE48D-syeZTefSF52_W5l3V67Gfv88vRnDI55q9FsVgubTR0BOSdukRgaCgYKAUUSARISFQF4udJhyjU_slB_qxA91wPQLhLAiw0163',
+#                                                               # 'refresh_token': '1//04qEH7eV1Cj9bCgYIARAAGAQSNwF-L9IrgHKkvHxdsKOtb5mtCWCAh3VDQXmiBmk2jdPfNnhM8v6cdjFjB9V0_k3fwDR34v8zFp0',
+#                                                               'client_id': '77864409271-ujcgeg2243mca6ek98pn7qahbjjhalga.apps.googleusercontent.com',
+#                                                               'client_secret': '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',
+#                                                               'token_uri': 'https://oauth2.googleapis.com/token'})
+#     headers = {
+#         'Authorization': f"Bearer {credentials.refresh_token}",
+#         'Content-Type': 'application/json; charset=utf-8'
+#     }
+#
+#     url = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
+#
+#     with open(file_path, 'rb') as file:
+#         data = file.read()
+#     response = requests.post(url, headers=headers, data=data)
+#     return response
+#
