d389e166161e03030356664736438bf1a7a81a8c
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-02 00:44:24 +0300

    update imports+errors E501

--- fixtures/constants.py
+++ fixtures/constants.py
@@ -1,12 +1,15 @@
 class Links:
-    CLIENT_ID = "77864409271-ujcgeg2243mca6ek98pn7qahbjjhalga.apps.googleusercontent.com"
+    CLIENT_ID = "77864409271-ujcgeg2243mca6ek98pn7qahbjjhalga." \
+                "apps.googleusercontent.com"
     CLIENT_SECRET = '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'
     REDIRECT_URI = 'http://localhost:8080'
     SCOPE = 'https://www.googleapis.com/auth/drive'
-    REFRESH_TOKEN = '1//0c_QMjblQzhrECgYIARAAGAwSNwF-L9Ir8BdP8rZPXUf07o0a6OQHbLbIsX8wjRvuC4YzHp007JB0s42CdFhTYjFOJRHVIGgUgRM'
+    REFRESH_TOKEN = '1//' \
+                    '0c_QMjblQzhrECgYIARAAGAwSNwF-L9Ir8BdP8rZPXUf07o0a6OQHbLbIsX8wjRvuC4YzHp007JB0s42CdFhTYjFOJRHVIGgUgRM'
     GET_TOKEN = 'https://accounts.google.com/o/oauth2/auth?'
     TOKEN_URL = 'https://oauth2.googleapis.com/token'
-    URL_DOWNLOAD = "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart"
+    URL_DOWNLOAD = "https://www.googleapis.com/upload/" \
+                   "drive/v3/files?uploadType=multipart"
     FILE_NAME = "input.txt"
     PARENTS = "1Geg7D6y-7wueDGxoGFFtDfvjrVUDTVwr"
     FILE_ID = 'WEn4zGvB7fCo0Ew6WYJpL2KPhH'
