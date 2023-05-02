406021ceb74885252626c961bf48202fa1e6173d
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-02 12:13:52 +0300

    update delete-test

--- tests/delete_file/test_delete_func.py
+++ tests/delete_file/test_delete_func.py
@@ -13,6 +13,7 @@ import allure
 def test_delete_file(update_refresh_token):
     value=delete_file(update_refresh_token,upload_file(update_refresh_token,Links.URL_DOWNLOAD,Links.URL_CHECK).json()["id"])
     assert value.status_code==204, "Check your file ID"
+
 @allure.feature("Проверка удаления файла")
 @allure.story("Проверка удалился ли файл 'input.txt' с неверным ID")
 @pytest.mark.negative
