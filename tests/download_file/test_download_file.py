c890aa6449f08a45cc34ba8f3b562a581931f25e
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-02 13:30:19 +0300

    add allure-steps+negative tests

--- tests/download_file/test_download_file.py
+++ tests/download_file/test_download_file.py
@@ -1,4 +1,3 @@
-
 import pytest
 from fixtures.upload.api import upload_file
 from fixtures.download.api import download_file
@@ -8,5 +7,16 @@ from fixtures.constants import Links
 @allure.story("Проверка функции скачивания файла 'input.txt' в текущую директорию")
 @pytest.mark.positive
 def test_download_file(update_refresh_token):
-    value=download_file(update_refresh_token,upload_file(update_refresh_token,Links.URL_DOWNLOAD,Links.URL_CHECK).json()["id"])
-    assert value.status_code == 200, "Check your file ID"
+    with allure.step("Скачивание файла с Google Drive"):
+        value=download_file(update_refresh_token,upload_file(update_refresh_token,Links.URL_DOWNLOAD,Links.URL_CHECK).json()["id"])
+    with allure.step("Запрос отправлен, посмотрим код ответа"):
+        assert value.status_code == 200, "Check your file ID"
+
+@allure.feature("Проверка скачивания файла")
+@allure.story("Проверка функции скачивания файла 'input.txt' с неизвестным ID в текущую директорию")
+@pytest.mark.negative
+def test_download_unknown_file(update_refresh_token):
+    with allure.step("Скачивание файла по неизвестной ссылке с Google Drive"):
+        value=download_file(update_refresh_token,Links.FILE_ID)
+    with allure.step("Запрос отправлен, посмотрим код ответа"):
+        assert value.status_code == 404, "Check your file ID"
