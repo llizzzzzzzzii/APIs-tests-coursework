98ecb1beb6e5dbad8a16f7c0268123c140cf0038
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-02 13:35:31 +0300

    update test_download_file.py

--- tests/download_file/test_download_file.py
+++ tests/download_file/test_download_file.py
@@ -4,17 +4,18 @@ from fixtures.download.api import download_file
 import allure
 from fixtures.constants import Links
 @allure.feature("Проверка скачивания файла")
-@allure.story("Проверка функции скачивания файла 'input.txt' в текущую директорию")
+@allure.story("Проверка функции скачивания файла 'input.txt'")
 @pytest.mark.positive
 def test_download_file(update_refresh_token):
     with allure.step("Скачивание файла с Google Drive"):
-        value=download_file(update_refresh_token,upload_file(update_refresh_token,
-                                                             Links.URL_DOWNLOAD,Links.URL_CHECK).json()["id"])
+        value=download_file(update_refresh_token,upload_file(
+            update_refresh_token,
+            Links.URL_DOWNLOAD,Links.URL_CHECK).json()["id"])
     with allure.step("Запрос отправлен, посмотрим код ответа"):
         assert value.status_code == 200, "Check your file ID"
 
 @allure.feature("Проверка скачивания файла")
-@allure.story("Проверка функции скачивания файла 'input.txt' с неизвестным ID в текущую директорию")
+@allure.story("Проверка функции скачивания файла 'input.txt' с неизвестным ID")
 @pytest.mark.negative
 def test_download_unknown_file(update_refresh_token):
     with allure.step("Скачивание файла по неизвестной ссылке с Google Drive"):
