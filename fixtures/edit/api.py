2c6c9a1282a5a36bc5ffa1d21c809274575274e5
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-04 09:33:20 +0300

    add edit_file test

---
+++ fixtures/edit/api.py
@@ -0,0 +1,9 @@
+from fixtures.constants import Links
+import requests
+from fixtures.request import Request
+def edit_file():
+    url = f"{Links.URL_UPLOAD}{Links.DOWNLOAD_FILE_ID}"
+    headers = {"Authorization": f"Bearer {Links.ACCESS_TOKEN}", "Content-Type": "text/plain"}
+    data = "test file"
+    response = requests.patch(url, headers=headers, data=data)
+    return response
---
+++ tests/edit_file/test_edit_file.py
@@ -0,0 +1,16 @@
+import pytest
+from fixtures.edit.api import edit_file
+import allure
+from fixtures.response import Response
+
+@allure.feature("Проверка изменения содержимого файла")
+@allure.story("Проверка функции изменения содержимого файла")
+@pytest.mark.positive
+def test_edit_file(update_refresh_token):
+    with allure.step("Изменение содержимого файла на Google Drive"):
+        value = edit_file()
+    with allure.step("Запрос отправлен, посмотрим код ответа"):
+        Response.log_assert(value.status_code == 200, "Check your file ID")
+    with allure.step("Проверим имя файла, который изменяли"):
+        Response.log_assert(value.json().get('name') == "test.txt", "Check your folder ID")
+
