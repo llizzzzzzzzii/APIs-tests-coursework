3f00420c4a0e17400823f8b0e66d8b040ddf1085
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-04 10:28:41 +0300

    add negative test

--- fixtures/add_folder/api.py
+++ fixtures/add_folder/api.py
@@ -1,13 +1,13 @@
-import requests
+
 from fixtures.constants import Links
 from fixtures.request import Request
 
-def add_new_folder():
+def add_new_folder(parents):
     response = Request("Add new folder").send_request(method="POST",
-                                                      url='https://www.googleapis.com/drive/v3/files',
+                                                      url=f"{Links.URL_CHECK}",
                                                       headers={'Authorization': f'Bearer {Links.ACCESS_TOKEN}',\
                                                                'Content-Type': 'application/json'},
                                                       json={'name': Links.FOLDER_NAME,
-                                                            'parents': [Links.PARENTS],
+                                                            'parents': [parents],
                                                             'mimeType': 'application/vnd.google-apps.folder'})
     return response
--- tests/add_new_folder/test_add_folder.py
+++ tests/add_new_folder/test_add_folder.py
@@ -1,5 +1,6 @@
 import pytest
 from fixtures.add_folder.api import add_new_folder
+from fixtures.constants import Links
 import allure
 from fixtures.response import Response
 
@@ -8,9 +9,18 @@ from fixtures.response import Response
 @pytest.mark.positive
 def test_add_folder(update_refresh_token):
     with allure.step("Создание папки на Google Drive"):
-        value = add_new_folder()
+        value = add_new_folder(Links.PARENTS)
     with allure.step("Запрос отправлен, посмотрим код ответа"):
-        Response.log_assert(value.status_code == 200, "Check your folder ID")
+        Response.log_assert(value.status_code == 200, "Check your parents ID")
     with allure.step("Проверим имя папки"):
-        Response.log_assert(value.json().get('name') == "test_folder", "Check your folder ID")
+        Response.log_assert(value.json().get('name') == "test_folder", "Check your parents ID")
+
+@allure.feature("Проверка создания файла")
+@allure.story("Проверка функции создания папки по неверной ссылке ")
+@pytest.mark.positive
+def test_search_unknown_file(update_refresh_token):
+    with allure.step("Создание папки по неверной ссылке на Google Drive"):
+        value = add_new_folder('')
+    with allure.step("Запрос отправлен, посмотрим код ответа"):
+        Response.log_assert(value.status_code == 404, "Check parents ID")
 
