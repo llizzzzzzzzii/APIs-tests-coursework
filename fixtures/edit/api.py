c8b2a352b4c54f7e708ff402ad8d4c43ea7586be
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-04 10:41:45 +0300

    add negative test

--- fixtures/edit/api.py
+++ fixtures/edit/api.py
@@ -1,9 +1,9 @@
 from fixtures.constants import Links
 import requests
 from fixtures.request import Request
-def edit_file():
-    url = f"{Links.URL_UPLOAD}{Links.EDIT_FILE_ID}"
+def edit_file(file_id):
+    url = f"{Links.URL_UPLOAD}{file_id}"
     headers = {"Authorization": f"Bearer {Links.ACCESS_TOKEN}", "Content-Type": "text/plain"}
-    data = "test file"
+    data = "testt file"
     response = requests.patch(url, headers=headers, data=data)
     return response
--- tests/edit_file/test_edit_file.py
+++ tests/edit_file/test_edit_file.py
@@ -1,4 +1,5 @@
 import pytest
+from fixtures.constants import Links
 from fixtures.edit.api import edit_file
 import allure
 from fixtures.response import Response
@@ -8,9 +9,17 @@ from fixtures.response import Response
 @pytest.mark.positive
 def test_edit_file(update_refresh_token):
     with allure.step("Изменение содержимого файла на Google Drive"):
-        value = edit_file()
+        value = edit_file(Links.EDIT_FILE_ID)
     with allure.step("Запрос отправлен, посмотрим код ответа"):
         Response.log_assert(value.status_code == 200, "Check your file ID")
     with allure.step("Проверим имя файла, который изменяли"):
-        Response.log_assert(value.json().get('name') == "test_edit_file.txt", "Check your folder ID")
+        Response.log_assert(value.json().get('name') == "test_edit_file.txt", "Check your file ID")
 
+@allure.feature("Проверка изменения содержимого файла")
+@allure.story("Проверка функции изменения содержимого файла с неверным ID")
+@pytest.mark.negative
+def test_edit_unknown_file(update_refresh_token):
+    with allure.step("Изменение содержимого файла с неверным ID на Google Drive"):
+        value = edit_file(Links.FILE_ID)
+    with allure.step("Запрос отправлен, посмотрим код ответа"):
+        Response.log_assert(value.status_code == 404, "Check your file ID")
