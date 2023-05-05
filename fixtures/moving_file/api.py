738d628019fd7a6c25eeda48d636c10230a5ed40
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-05 18:15:29 +0300

    update moving test

--- fixtures/moving_file/api.py
+++ fixtures/moving_file/api.py
@@ -1,9 +1,8 @@
 from fixtures.constants import Links
 from fixtures.request import Request
-
 def moving_file(file_id, new_parents):
     response = Request("Moving file").send_request(method="PATCH",
-                                                   url=f'https://www.googleapis.com/drive/v3/files/{file_id}?'
+                                                   url=f'{Links.URL_CHECK}{file_id}?'
                                                        f'addParents={new_parents}&removeParents={Links.PARENTS}',
                                                    headers={'Authorization': f'Bearer {Links.ACCESS_TOKEN}',
                                                             'Content-Type': 'application/json'})
--- tests/moving_file/test_moving_file.py
+++ tests/moving_file/test_moving_file.py
@@ -3,7 +3,6 @@ from fixtures.moving_file.api import moving_file
 import allure
 from fixtures.response import Response
 from fixtures.constants import Links
-from fixtures.add_folder.api import add_new_folder
 from fixtures.upload.api import upload_file
 
 @allure.feature("Проверка перемещения файла")
@@ -11,7 +10,7 @@ from fixtures.upload.api import upload_file
 @pytest.mark.positive
 def test_moving_file(update_refresh_token, get_file_id):
     with allure.step("Перемещение файла на Google Drive"):
-        value = moving_file(upload_file(Links.URL_CHECK).json()["id"], add_new_folder(Links.PARENTS).json()["id"])
+        value = moving_file(upload_file(Links.URL_CHECK).json()["id"], Links.FOLDER_FOR_MOVING)
     with allure.step("Запрос отправлен, посмотрим код ответа"):
         Response.log_assert(value.status_code == 200, "Check your parents ID")
 
@@ -19,7 +18,7 @@ def test_moving_file(update_refresh_token, get_file_id):
 @allure.story("Проверка функции перемещения файла")
 @pytest.mark.negative
 def test_moving_unknown_file(update_refresh_token, get_file_id):
-    with allure.step("Перемещение несуществующего файла на Google Drive"):
-        value = moving_file(Links.FILE_ID, add_new_folder(Links.PARENTS).json()["id"])
+    with allure.step("Перемещение несуществующего файла в несуществующую папку на Google Drive"):
+        value = moving_file(Links.FILE_ID, Links.PARENTS_INCORR)
     with allure.step("Запрос отправлен, посмотрим код ответа"):
         Response.log_assert(value.status_code == 404, "Check your parents ID")
