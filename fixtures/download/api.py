b5fbffd24b82db5df7877b1790ae2764e13e5557
llizzzzzzzzii <liza.zakharowa2003yandex.ru>
2023-05-15 16:31:24 +0300

    update download test

--- fixtures/download/api.py
+++ fixtures/download/api.py
@@ -6,6 +6,6 @@ def download_file(file_id):
                        url=f'{Links.URL_CHECK}{file_id}?alt=media',
                        headers={"Authorization": f"Bearer {Links.ACCESS_TOKEN}"})
     
-    with open(Links.DOWNLOAD_FILE_NAME, 'wb') as f:
+    with open(f'{Links.FOLDER_PATH}/{Links.DOWNLOAD_FILE_NAME}','wb') as f:
         f.write(response.content)
     return response
